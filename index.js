45436e290daab8123fad5ee7a0772aeb9dc241f5
Sairaj Morajkar <saimorajkar123@gmail.com>
2024-04-02 03:46:54 +0530

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function toggleMenu(){
     const menu = document.querySelector('.menu-links');
     const icon = document.querySelector('.hamburger-icon');
-    menu.classList.toggle('opean');
-    icon.classList.toggle('opean');
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
 }
